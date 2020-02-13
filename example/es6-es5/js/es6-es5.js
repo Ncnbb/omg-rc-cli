31dc30bf9540aae0e436e214de06c3632bdafd80
LLLLLamHo <linhao.4zhong@163.com>
2020-02-13 17:23:58 +0800

    完成入口配置

---
+++ example/es6-es5/js/es6-es5.js
@@ -0,0 +1,8 @@
+const THIS_IS_CONST = 'const';
+let this_is_let = 'let';
+
+const say = () => {
+    console.log('hi, i\'m omg-cli');
+}
+
+say();
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const program = require( 'commander' );
 const pkg = require( './package.json' );
-const logoOutput = require( './script/module/logo/logo' );
-const createDir = require( './script/module/create/create' );
-const errlog = require( './script/extension/console/errorLog' );
-const warnlog = require( './script/extension/console/warnlog' );
-const commandDone = require('./script/extension/console/command_done');
+const logoOutput = require( './lib/modules/logo/logo' );
+const createDir = require( './lib/script/create' );
+const warnlog = require( './lib/extension/console/warnlog' );
+const commandDone = require( './lib/extension/console/command_done' );
+const build = require('./lib/script/build');
 
 program
     .version( pkg.version, '-v, --version' )
     .usage( '[option] | omg [command]' )
-    .action(() => {
+    .action( () => {
         // 没有命中任何命令
-        warnlog('Your current command does not exist, please read the documentation carefully!');
-        warnlog('You can enter the following command：omg --help');
-    })
+        warnlog( 'Your current command does not exist, please read the documentation carefully!' );
+        warnlog( 'You can enter the following command：omg --help' );
+    } )
 
 program
     .command( 'create <dirPath> <name>' )
@@ -21,13 +21,27 @@ program
     .action( async ( dirPath, name ) => {
         const result = await createDir( dirPath, name );
         if ( result ) {
-            commandDone( 'omg create task done!' );
+            commandDone( 'ok', 'omg create task done!' );
         } else {
-            errlog( 'omg create task error!' );
+            commandDone( 'err', 'omg create task error!' );
+        }
+        process.exit( 1 );
+    } );
+
+program
+    .command( 'build [targetPath] [targetDir]' )
+    .description( 'build file, [targetPath] specify build directory, [targetDir] specified file in dirTarget.' )
+    .action( async ( path, dir ) => {
+        const result = await build( path, dir );
+        if ( result ) {
+            commandDone( 'ok', 'omg build task done!' );
+        } else {
+            commandDone( 'err', 'omg build task error!' );
         }
         process.exit( 1 );
     } );
 
 program.parse( process.argv );
 // 默认输出
-if (!program.args || program.args.length == 0) console.log( logoOutput() )
+// if ( !program.args || program.args.length == 0 ) console.log( logoOutput() )
+if ( !process.argv ||  process.argv.length == 2 ) console.log( logoOutput() )
