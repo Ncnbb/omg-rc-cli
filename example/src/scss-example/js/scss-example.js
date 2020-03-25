2296d5baa53599ed64b54042833112dd7c531786
LLLLLamHo <linhao.4zhong@163.com>
2020-03-25 17:46:43 +0800

    修复css-loader的rule使用错误

--- example/src/scss-example/js/scss-example.js
+++ example/src/scss-example/js/scss-example.js
@@ -3,12 +3,14 @@ import '../style/index.scss';
 import styles from '../style/index.module.scss';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'zzc-design-mobile/lib/Button';
 
 class App extends Component {
     render () {
         return (
             <div>
                 <h1>OMG-CLI</h1>
+                <Button>111143334</Button>
                 <p className={styles['class-name1']}>this is text</p>
             </div>
         )
