efe3ed85148b95f0b780e7e288c506c4ce51a5b2
LLLLLamHo <linhao.4zhong@163.com>
2020-09-09 18:06:23 +0800

    test

--- example/dist/ts/tsx-js-example/js/sub.d.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from 'react';
-export default class Sub extends Component<any, any> {
-    render(): JSX.Element;
-}
--- example/src/scss-example/js/scss-example.js
+++ example/src/scss-example/js/scss-example.js
@@ -10,7 +10,7 @@ class App extends Component {
         return (
             <div>
                 <h1>OMG-CLI</h1>
-                <Button>111143334</Button>
+                <Button>11333412123</Button>
                 <p className={styles['class-name1']}>this is text</p>
             </div>
         )
